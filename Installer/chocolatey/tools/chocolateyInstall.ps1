d067da329fd3978ef5ff800fa65aeb04ee13119e
mitul002 <mitul15-4617@diu.edu.bd>
2026-06-01 01:38:51 +0600

    fix: updated checksums for the final compiled exe and website exe

--- Installer/chocolatey/tools/chocolateyInstall.ps1
+++ Installer/chocolatey/tools/chocolateyInstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
   silentArgs    = '/SILENT /NORESTART /SP-'
   validExitCodes= @(0)
   softwareName  = 'Clipboard Pro'
-  checksum      = 'F22D0046D849DECD16FA7032FED4CF0DA2566FC0BF24B96A91CF6E55DA86D803'
+  checksum      = 'A433013A85311FB8D17CC9FBE03B09159EFFC9267D633F9CCB70E9C936CA5AD2'
   checksumType  = 'sha256'
 }
 
